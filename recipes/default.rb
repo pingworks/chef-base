718c7cc674bc67f09de54f802c4bf5a64fda8830
Christoph Lukas <lukas@pingworks.de>
2015-09-24 14:47:28 +0200

    changed /etc/hosts to resolv correct hostname, extended resolv.conf searchdomains

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,11 +3,24 @@ bash 'write hostsfile' do
   group 'root'
   cwd '/root'
   code <<-EOF
-  echo "127.0.0.1 localhost $(hostname)" > /etc/hosts
-  echo "$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/' ) #{node['ws-base']['cname']}.#{node['ws-base']['domain']} #{node['ws-base']['cname']}" >> /etc/hosts
+  echo "127.0.0.1 localhost" > /etc/hosts
+  echo "$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/' ) #{node['ws-base']['cname']}.#{node['ws-base']['domain']} #{node['ws-base']['cname']} $(hostname)" >> /etc/hosts
   EOF
   #not_if 'test -s /etc/hosts'
 end
+
+# Calculate a list of searchdomains for dns requests from
+# the actual domain and the basedomain
+domain = node['ws-base']['domain']
+basedomain = node['ws-base']['basedomain']
+domainparts = domain[0, domain.length-basedomain.length-1].split('.').reverse
+searchdomains = [basedomain]
+base = ""
+domainparts.each{ |part|
+  searchdomains.push(part + "." + base + basedomain)
+  base+=part + "."
+}
+searchdomainstring = searchdomains.reverse.join(' ')
 
 bash 'write resolv.conf' do
   user 'root'
@@ -16,7 +29,7 @@ bash 'write resolv.conf' do
   code <<-EOF
   echo 'nameserver #{node['ws-base']['dns']}' > /etc/resolv.conf
   echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
-  echo 'search #{node['ws-base']['domain']}' >> /etc/resolv.conf
+  echo 'search #{searchdomainstring}' >> /etc/resolv.conf
   EOF
   #not_if 'test -s /etc/resolv.conf'
 end
