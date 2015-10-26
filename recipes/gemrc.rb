a081d87b5a5a737d5af9815e52d6f1fd99af8471
Christoph Lukas <lukas@pingworks.de>
2015-10-26 13:05:40 +0100

    create parent directory

--- recipes/gemrc.rb
+++ recipes/gemrc.rb
@@ -1,4 +1,12 @@
 node['pw_base']['gemrc-files'].each do |file|
+  directory File.dirname(file) do
+    owner 'root'
+    group 'root'
+    mode 00755
+    recursive true
+    action :create
+  end
+
   template file do
     source 'gemrc.erb'
     owner 'root'
